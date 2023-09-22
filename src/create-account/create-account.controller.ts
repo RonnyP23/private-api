1c28dfa44322fb4cbd3a6cbea1f4b7be43ed7c6a
RonnyP23 <ronnyana19966@gmail.com>
2023-09-21 22:45:09 -0300

    metodo deletar aprimorado e em pleno funcionamento

--- src/create-account/create-account.controller.ts
+++ src/create-account/create-account.controller.ts
@@ -49,13 +49,12 @@ export class CreateAccountController {
     }
 
     @Delete(':id') 
-    public async deleteAccount(@Param('id') id: number): Promise<{message: string}> {
-        const result = await this.repository.delete(id); 
-        if (result.affected === 1) {
-            return { message: 'Registro excluído com sucesso' };
-          } else {
-            return { message: 'Registro não encontrado ou não pôde ser excluído' };
-          }
+    public async deleteAccount(@Param('id') id: number): Promise<{ data: CreateAccountModel}> {
+      
+        const result = await this.repository.findOne({ where: { id }}); 
+         await this.repository.remove(result)
+
+        return {data: result}
         }
         
 }
