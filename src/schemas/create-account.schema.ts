1fa5defe5ef935334693212aed2f4027bc33d6e1
RonnyP23 <ronnyana19966@gmail.com>
2023-10-07 20:43:40 -0300

    implementado documentação automatica usando swaager

--- src/schemas/create-account.schema.ts
+++ src/schemas/create-account.schema.ts
@@ -1,29 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {IsEmail, IsNotEmpty, IsNumber, IsString} from 'class-validator';
 
 
 export class CreateAccountSchema {
+    @ApiProperty({
+        description: 'Nome de usuario',
+        example: 'Ronny paulo'
+    })
     @IsNotEmpty()
     @IsString()
     name: string;
 
+    @ApiProperty({
+        description: 'email de usuario',
+        example: 'Ronnypaulo@gmail.com'
+    })
     @IsNotEmpty()
     @IsEmail()
     @IsString()
     email: string;
 
-    
+    @ApiProperty()
     @IsString()
     telefone: string;
 
-    
+    @ApiProperty()
     @IsString()
     cpf: string;
 
+    @ApiProperty()
     @IsNotEmpty()
     @IsString()
     password: string;
-
-    @IsNumber()
-    userLevel: number;
-
 }
