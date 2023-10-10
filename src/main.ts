d0304a899b925e222fc043225a941d0321f09162
RonnyP23 <ronnyana19966@gmail.com>
2023-10-09 22:36:15 -0300

    configuração de cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {  SwaggerModule,DocumentBuilder } from '@nestjs/swagger';
+import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
+
+  const corsOptions = {
+    'Access-Control-Allow-Origin': 'https://br-anth-ro-angular-291699cb4ec2.herokuapp.com',
+    'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type,Application',
+     
+      
+    };
+  
+   app.use(cors(corsOptions));
 
   const config = new DocumentBuilder()
     .setTitle('Private api')
@@ -14,7 +25,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.enableCors();
+  
   await app.listen(4000);
 }
 bootstrap();
